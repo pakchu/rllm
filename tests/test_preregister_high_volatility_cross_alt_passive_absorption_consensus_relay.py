92bdaa8a4b2951f49ac93576c407e62cb1401ba6
pakchu <gus4734@gmail.com>
2026-08-13 05:12:21 +0900

    preregister cross-alt passive absorption consensus

---
+++ tests/test_preregister_high_volatility_cross_alt_passive_absorption_consensus_relay.py
@@ -0,0 +1,4 @@
+import json
+from training import preregister_high_volatility_cross_alt_passive_absorption_consensus_relay as p
+def test_registration_is_canonical_blind_and_frozen():
+ x=p.build();p.validate(x);h=x.pop('manifest_hash');assert p.canonical_hash(x)==h;assert x['policy_id']=='HVCAPAC-8' and not x['outcomes_opened'] and not x['source_incidence_opened'] and not x['gross9_rows_opened'];assert x['policy']['absorption_strength_rank_min']==.75 and x['policy']['minimum_consensus_breadth']==4 and x['policy']['variation_rank_min']==.65;assert x['stopping_rule'].startswith('terminal first failure');json.dumps(x,allow_nan=False)
