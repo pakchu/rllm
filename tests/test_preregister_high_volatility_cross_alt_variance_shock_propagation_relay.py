f5c5a4c07d07ad3038118d80070be6b8cd99c015
pakchu <gus4734@gmail.com>
2026-08-13 08:34:40 +0900

    preregister cross-alt variance shock propagation

---
+++ tests/test_preregister_high_volatility_cross_alt_variance_shock_propagation_relay.py
@@ -0,0 +1,5 @@
+from training import preregister_high_volatility_cross_alt_variance_shock_propagation_relay as s
+def test_blind_canonical_registration():
+ x=s.build();s.validate(x);assert x['manifest_hash']==s.canonical_hash({k:v for k,v in x.items() if k!='manifest_hash'});assert not x['outcomes_opened'] and not x['source_incidence_opened'] and not x['gross9_rows_opened']
+def test_fixed_variance_shock_policy():
+ x=s.build();assert len(s.ALTS)==6;assert x['policy']['shock_bars']==12 and x['policy']['minimum_shocked_alts']==4;assert x['policy']['decision_hours_utc']==[2,14];assert x['clock']['hold']=='8 elapsed hours';assert not x['research_boundary']['repair_of_prior_candidate']
